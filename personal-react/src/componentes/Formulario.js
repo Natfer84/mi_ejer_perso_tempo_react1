efee2283010c5557427efe5e2312da9ab0973753
Natfer84 <natita2804@hotmail.com>
2024-11-30 12:50:16 +0100

    eje personal tareas react

---
+++ personal-react/src/componentes/Formulario.js
@@ -0,0 +1,16 @@
+ import React from 'react';
+
+ function Formulario() {
+  return(
+  <div className='contenedor-div-formulario'>
+  <form className="contenedro-form">
+    <p className='titulo_tarea'>Formulario Tareas</p>
+    <input type="text" placeholder='Título de la tarea' className='input_titulo'></input>
+    <textarea className='descrip_tarea' placeholder='Descripción de la tarea'></textarea>
+    <br></br>
+    <input type='submit' value="ENVIAR TAREA" className='botonEnvTarea'></input>
+  </form>
+</div>
+) 
+}
+ export default Formulario;
