9507a640c9e82ff36f884c214dfb7edc13b0c30a
Natfer84 <natita2804@hotmail.com>
2024-11-30 14:48:40 +0100

    eje personal react

--- personal-react/src/App.js
+++ personal-react/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Formulario from "./componentes/Formulario.js";
+import FormularioTareastareas from "./componentes/Formulario-tareas.js";
 
 function App() {
   return (
     <div className="App">
        <h1 className='tiulo-app'>Bienvenido a tareas TO DO</h1>
       <Formulario />
+      <h2 className='tiulo-tareas-exis'>Listado de tareas existentes</h2>
+      <FormularioTareastareas />
     </div>
   );
 }
---
+++ personal-react/src/componentes/Formulario-tareas.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Formulario from "./Formulario";
+import '../hojas-de-estilo/Formulario.css';
+
+function FormularioTareas() {
+    return(
+    <div className='contenedor-formulario-tareas'>
+   <Formulario />
+  </div>
+  ) 
+  }
+   export default FormularioTareas;
