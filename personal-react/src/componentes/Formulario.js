9c6d2d981ce90018373ab9697f5676eef355402c
Natfer84 <natita2804@hotmail.com>
2024-12-01 11:48:51 +0100

    eje personal form react

--- personal-react/src/componentes/Formulario.js
+++ personal-react/src/componentes/Formulario.js
@@ -1,17 +1,29 @@
- import React from 'react';
- import '../hojas-de-estilo/Formulario.css';
+import React from "react";
+import "../hojas-de-estilo/Formulario.css";
 
- function Formulario() {
-  return(
-  <div className='contenedor-div-formulario'>
-  <form className="contenedro-form">
-    <p className='titulo_tarea'>Formulario Tareas</p>
-    <input type="text" placeholder='Título de la tarea' className='input_titulo'></input>
-    <textarea className='descrip_tarea' placeholder='Descripción de la tarea'></textarea>
-    <br></br>
-    <input type='submit' value="ENVIAR TAREA" className='botonEnvTarea'></input>
-  </form>
-</div>
-) 
+function Formulario() {
+
+  return (
+    <div className="contenedor-div-formulario">
+      <form className="contenedro-form">
+        <p className="titulo_tarea">Formulario Tareas</p>
+        <input
+          type="text"
+          placeholder="Título de la tarea"
+          className="input_titulo"
+        ></input>
+        <textarea
+          className="descrip_tarea"
+          placeholder="Descripción de la tarea"
+        ></textarea>
+        <br></br>
+        <input
+          type="submit"
+          value="ENVIAR TAREA"
+          className="botonEnvTarea"
+        ></input>
+      </form>
+    </div>
+  );
 }
- export default Formulario;
+export default Formulario;
